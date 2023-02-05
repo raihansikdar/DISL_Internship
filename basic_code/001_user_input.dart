b70dbfd6284a37028fe9e91bd387989d4ef2f13a
RaihanSikdar <raihansikdar10@gmail.com>
2023-02-05 17:48:27 +0600

    1st day

---
+++ basic_code/001_user_input.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  int? n = int.parse(stdin.readLineSync()!); // Here ? and ! are for null safety
+  print("user number is ${n}");
+
+  String? name = stdin.readLineSync();
+  print("user name is ${name}");
+
+}
---
+++ basic_code/008_prime_number.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main() {
+  int? n = int.parse(stdin.readLineSync()!);
+  var flag = 0;
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag != 0) {
+    print("Not Prime");
+  } else
+    print("prime");
+}
