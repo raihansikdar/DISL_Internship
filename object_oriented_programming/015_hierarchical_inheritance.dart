161af8d98f52fd3f96352144241cdb76366ce3c2
RaihanSikdar <raihansikdar10@gmail.com>
2023-02-12 17:25:02 +0600

    inheritance

---
+++ object_oriented_programming/015_hierarchical_inheritance.dart
@@ -0,0 +1,48 @@
+ class Person {  
+  void name(String name) {  
+    print(name);  
+  }  
+  
+  void id(int id) {  
+    print(id);  
+  }  
+}  
+  
+
+
+class Raihan extends Person {  
+   
+  void nationality(String nationality) {  
+    print(nationality);  
+  }  
+}   
+
+
+
+
+class Mamun extends Person {  
+    void result(String result){  
+      print(result);  
+} 
+
+
+} 
+
+
+
+void main() {  
+    // Creating Object of James class
+    
+    Raihan obj = new Raihan();  
+    obj.name("Raihan");  
+    obj.id(101);  
+    obj.nationality("bangladeshi"); 
+
+    Mamun objM = new Mamun();  
+    objM.name("Mamun");  
+    objM.id(104);  
+    objM.result("Passed");  
+  
+    // Creating Object of Peter class  
+   
+}  
